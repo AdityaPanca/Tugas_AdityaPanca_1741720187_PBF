e19f3ada82bf71b7cdd180037d849c810da5f679
AdityaPanca <aditya.annecdo@gmail.com>
2020-02-19 12:33:35 +0700

    finalisasi biodata

--- aditya-minggu3/src/App.js
+++ aditya-minggu3/src/App.js
@@ -4,22 +4,113 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div>
+  <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+  <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="#">Active</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link disabled" href="#">Disabled</a>
+    </li>
+  </ul>  
+</nav>
+
+<body>
+  <div class = "container">
+    <div class= "kartu">
+       <div class='row'>
+         <div class="col-md-4">
+           <div class = "foto">
+              <img src={require("../src/poltek.png")} alt="" width="300" height="auto"></img>
+           </div>
+         </div>
+         <div class="col-md-8 kertas-biodata">
+          <div class="biodata">
+          <table width="100%" border="0"> 
+          <div class="card">
+          <table class="table">
+            <tbody>
+              <tr>
+                <th scope="row"></th>
+                <th>Nama</th>
+                <td>:</td>
+                <td>Aditya Panca Putra</td>
+              </tr>
+              <tr>
+                <th scope="row"></th>
+                <th>TTL</th>
+                <td>:</td>
+                <td>Malang, 22 Maret 1999</td>
+              </tr>
+              <tr>
+                <th scope="row"></th>
+                <th>Jeni Kelamin</th>
+                <td>:</td>
+                <td>Laki-laki</td>
+              </tr>
+              <tr>
+                <th scope="row"></th>
+                <th>Alamat</th>
+                <td>:</td>
+                <td>Jl Gadang Gg4 no 67</td>
+              </tr>
+              <tr>
+                <th scope="row"></th>
+                <th>No. Hp</th>
+                <td>:</td>
+                <td>085330808062</td>
+              </tr>
+              <tr>
+                <th scope="row"></th>
+                <th>Status</th>
+                <td>:</td>
+                <td>Mahasiswa</td>
+                <td></td>
+              </tr>
+              <tr>
+                <th scope="row"></th>
+                <th>Hobi</th>
+                <td>:</td>
+                <td>Futsal</td>
+              </tr>
+              <tr>
+                <th scope="row"></th>
+                <th>Email</th>
+                <td>:</td>
+                <td>Aditya.annecdo@gmail.com</td>
+              </tr>
+              <tr>
+                <th scope="row"></th>
+                <th>Github</th>
+                <td>:</td>
+                <td>AdityaPanca</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+          </table>
+          </div>
+         </div>
+       </div>
     </div>
+  </div>
+
+
+
+</body>
+
+<footer>
+  <p>Aditya.annecdo@gmail.com</p>
+</footer>
+
+</div>
   );
 }
 
