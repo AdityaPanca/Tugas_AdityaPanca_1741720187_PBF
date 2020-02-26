25b23662c4dabf9b47f73240b7d85392ae9abde5
AdityaPanca <aditya.annecdo@gmail.com>
2020-02-26 11:10:45 +0700

    update css

--- aditya-minggu4/src/component/login.jsx
+++ aditya-minggu4/src/component/login.jsx
@@ -3,31 +3,32 @@ import './login.css';
 
 const LoginForm = () => {
     return (
-     <div class="kotak_login">
+    <div className="home">
      <div class="judul">
-
-       <p class="tulisan_login">Tugas Pertemuan Ketiga</p>
-
-       <div>
+       <h2>Form Login</h2>
+      <div class="kotak_login">
+       <p><h1>Tugas Pertemuan</h1></p>
+      <p><h1>Ketiga</h1></p>
          <form>
         <label>Username</label>
         <input type="text" name="username" class="form_login" placeholder="Username atau email .."/>
+        <br></br>
         <label>Password</label>
 		    <input type="text" name="password" class="form_login" placeholder="Password .."/>
 
         <input type="submit" class="tombol_login" value="LOGIN"></input>
-
-        <input type="checkbox"/>Remember Me
+        <br></br>
+          <input type="checkbox"/>Remember Me
+          <br></br>
+        
 
 		    <br/>
         <input type="submit" class="tombol_cancel" value="CANCEL"></input>
         </form>
        </div>
      </div>
-       
-
+</div>
         
-     </div>
   );
 };
 
