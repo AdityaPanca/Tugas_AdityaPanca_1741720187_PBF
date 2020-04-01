7228b7d32983ce93eb128e98810f8122aa8175ad
AdityaPanca <aditya.annecdo@gmail.com>
2020-04-01 15:23:02 +0700

    UTS React

--- aditya-minggu6/src/App.js
+++ aditya-minggu6/src/App.js
@@ -1,150 +1,90 @@
-import React from "react";
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import CreateTodo from './containers/CreateTodo'
+import Table from './containers/Table'
+import gambar5 from './gambar1.jpg';
+import gambar1 from './4.jpg';
+import gambar2 from './5.jpg';
+import gambar3 from './6.jpg';
+import tools from './tools.png';
+import t1 from './t1.png';
+import t2 from './t2.png';
+import makeup from './makeup.png';
+import satu from './satu.png';
+import dua from './dua.png';
+import tiga from './tiga.png';
+import home1 from './home1.png';
+import home2 from './home2.png';
+import home3 from './home3.png';
+import home4 from './home4.png';
+import home5 from './home5.png';
+import alis from './alis.jpg';
+import bedak from './bedak.jpg';
+import perona from './perona.jpg';
+
+
 import {
   BrowserRouter as Router,
+  Switch,
   Route,
   Link,
-  useHistory,
   useLocation,
   Redirect,
-  Switch
+  useHistory
 } from "react-router-dom";
+import { logDOM } from '@testing-library/react';
 
-export default function App() {
-  return (
-    <Router>
-      <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="#">
-            CatsStore
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <Link className="nav-link" to="/">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/product">
-                  Products
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/cart">
-                  Cart
-                </Link>
-              </li>
-              {/* <button className="nav-item">
-                <Link className="nav-link" to="/login">
-                  Login
-                </Link>
-              </button> */}
-            </ul>
-          </div>
-        </nav>
-
-        <Route path="/" component={Home} exact />
-        <PrivateRoute path="/product" component={Product} />
-        <Route path="/cart" component={Cart} />
-        <Route path="/login" component={Login} />
-      </div>
-    </Router>
-  );
-}
-
-function Cart() {
-  return <div>Cart</div>;
-}
-
-function Home() {
-  return <div>Home</div>;
-}
-
-function Product({ children, ...rest }) {
-  return (
-    <div>
-      <h1>Products</h1>
-      <div className="row">
-        <div className={"col-4"}>
-          <div class="card">
-            <img src={"/img/1.png"} class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-text">Card Title</h5>
-              <p>Price 100$</p>
-
-              <a href="#" class="btn btn-primary">
-                Details
-              </a>
-            </div>
-          </div>
-
-          <div class="card">
-            <img src={"/img/1.png"} class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-text">Card Title</h5>
-              <p>Price 100$</p>
-
-              <a href="#" class="btn btn-primary">
-                Details
-              </a>
-            </div>
-          </div>
-
-          <div class="card">
-            <img src={"/img/1.png"} class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-text">Card Title</h5>
-              <p>Price 100$</p>
-
-              <a href="#" class="btn btn-primary">
-                Details
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <Route
-        {...rest}
-        render={({ location }) =>
-          fakeAuth.isAuthenticated ? (
-            children
-          ) : (
-            <Redirect
-              to={{
-                pathname: "/login",
-                state: { from: location }
-              }}
-            />
-          )
-        }
-      />
-
-    </div>
-  );
-}
-
-function Login() {
+export default function AuthExample() {
   return (
     <Router>
       <div>
-        <AuthButton />
 
+        <AuthButton />
+        <div>
+          <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+            <a className="navbar-brand" href="#">SEPHORA</a>
+
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav">
+                <a className="nav-item nav-link active" href="/public">Home <span className="sr-only">(current)</span></a>
+                <a className="nav-item nav-link active" href="/login">Makeup</a>
+                <a className="nav-item nav-link active" href="/skincare">Skincare</a>
+                <a className="nav-item nav-link active" href="/loginn">Login</a>
+
+              </div>
+            </div>
+          </nav></div>
+        {/* <ul>
+          <li>
+            <Link to = "/public">Public page</Link>
+          </li>
+          <li>
+            <Link to = "/private">Private page</Link>
+          </li>
+          <li>
+            <Link to = "/skincare">Skincare page</Link>
+          </li>
+        </ul> */}
         <Switch>
-          <Route path="/login">
-            <LoginPage />
+          <Route path="/public">
+            <PublicPage />
           </Route>
+          <Route path="/loginn">
+            <LoginnPage />
+          </Route>
+          {/* <Route path="/login">
+            <LoginPage />
+          </Route> */}
+          <Route path="/skincare">
+            <SkincarePage />
+          </Route>
+          <PrivateRoute path="/private">
+            <ProtectedPage />
+          </PrivateRoute>
         </Switch>
       </div>
     </Router>
@@ -153,7 +93,7 @@ function Login() {
 
 const fakeAuth = {
   isAuthenticated: false,
-  authenticate(cb) {
+  Authenticate(cb) {
     fakeAuth.isAuthenticated = true;
     setTimeout(cb, 100);
   },
@@ -163,24 +103,38 @@ const fakeAuth = {
   }
 };
 
-function AuthButton() {
+function LoginnPage(){
   let history = useHistory();
+  let location = useLocation();
 
-  return fakeAuth.isAuthenticated ? (
-    <p>
-      Welcome!{" "}
-      <button
-        onClick={() => {
-          fakeAuth.signout(() => history.push("/"));
-        }}
-      >
-        Sign Out
-      </button>
-    </p>
-  ) : (
-    <p>You are not logged in.</p>
+  let { from } = location.state || { from: { pathname: "/loginn" } };
+  let login = () => {
+    fakeAuth.authenticate(() => {
+      history.replace(from);
+    });
+  };
+
+  return (
+    <div>
+      <p>SELAMAT DATANG DI SEPHORA!{from.pathname}</p>
+      <button onClick={login}>Log in</button>
+    </div>
   );
 }
+function AuthButton() {
+  let history = useHistory();
+  return fakeAuth.isAuthenticated ? (
+    <p>
+      Terimakasih Telah Berkunjung Ke SEPHORA{""}
+      <button onClick={() => { fakeAuth.signout(() => history.push("/loginn")); }}>Back To Home</button>
+    </p>
+  ) : (
+      <p>
+
+      </p>
+    );
+}
+
 
 function PrivateRoute({ children, ...rest }) {
   return (
@@ -190,33 +144,205 @@ function PrivateRoute({ children, ...rest }) {
         fakeAuth.isAuthenticated ? (
           children
         ) : (
-          <Redirect
-            to={{
-              pathname: "/",
-              state: { from: location }
-            }}
-          />
-        )
+            <Redirect
+              to={{
+                pathname: "/loginn",
+                state: { from: location }
+              }}
+            />
+          )
       }
     />
   );
 }
 
+function PublicPage() {
+  return <div className="card text-center">
+    <div className="card-header">
+      SEPHORA
+  </div>
+    <div className="card-body">
+    <center>
+        <img className="img-top" src={home1} alt="Card cap" />
+        </center>
+        <center>
+        <img className="img-top" src={home2} alt="Card cap" />
+        </center>
+        <center>
+        <img className="img-top" src={home3} alt="Card cap" />
+        </center>
+        <center>
+        <img className="img-top" src={home4} alt="Card cap" />
+        </center>
+        
+      <h5 className="card-title">About Sephora</h5>
+      <p className="card-text">Sephora is a visionary beauty-retail concept founded in France by Dominique Mandonnaud in 1970. Sephora's unique, open-sell environment features an ever-increasing amount of classNameic and emerging brands across a broad range of product categories including skincare, makeup, fragrance, body and hair care, in addition to Sephora's own private label. </p>
+      <p className="card-text"> Today, Sephora is not only the leading chain of perfume and cosmetics stores in France, but also a powerful beauty presence in countries around the world.</p>
+      <p className="card-text">Owned by LVMH Moët Hennessy Louis Vuitton, the world's leading luxury goods group, Sephora is highly regarded as a beauty trailblazer, thanks to its unparalleled assortment of prestige products, unbiased service from experts, interactive shopping environment, disruptive spirit and constant innovation.</p>
+      <p className="card-text">Sephora operates approximately 1,900 stores in 29 countries worldwide, with an expanding base of over 200 stores across the Asia Pacific region including Australia, China, Singapore, Malaysia, Thailand, Indonesia and India.</p>
+    </div>
+    <center>
+        <img className="img-top" src={home5} alt="Card cap" />
+        </center>
+  </div>;
+}
+function SkincarePage() {
+  return (
+    <div className="card text-center">
+      <div className="card-header">
+        SEPHORA
+  </div>
+      <div className="container" style={{ marginTop: "80px" }} >
+        <center>
+          <img className="img-catalog" src={gambar5} alt="Card cap" />
+        </center>
+        <center>
+        <img className="img-top" src={makeup} alt="Card cap" />
+        </center>
+        <center>
+        <img className="img-top" src={satu} alt="Card cap" />
+        </center>
+        <center>
+        <img className="img-top" src={dua} alt="Card cap" />
+        </center>
+        <center>
+        <img className="img-top" src={tiga} alt="Card cap" />
+        </center>
+        <center>
+        <img className="img-top" src={tools} alt="Card cap" />
+        </center>
+        <center>
+        <img className="img-top" src={t1} alt="Card cap" />
+        </center>
+        <center>
+        <img className="img-top" src={t2} alt="Card cap" />
+        </center>
+        <div className="row">
+          <CreateTodo />
+          <div className="col-lg-10 offset-lg-2 col-md-10 col-sm-12 col-xs-12">
+
+          </div>
+          <Table />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+function ProtectedPage() {
+  return <h3>Private</h3>;
+}
 function LoginPage() {
   let history = useHistory();
   let location = useLocation();
-
-  let { from } = location.state || { from: { pathname: "/product" } };
+  let { from } = location.state || { from: { pathname: "/loginn" } };
   let login = () => {
-    fakeAuth.authenticate(() => {
+    fakeAuth.Authenticate(() => {
       history.replace(from);
     });
   };
+  return (
+    <div className="card-deck" style={{width:  'max-content', height: 'max-content'}}>
+      <div className="card">
+        <img className="img-catalog" src={gambar1} alt="Card cap" />
+        <div className="card-body">
+          <h5 className="card-title">URBAN DECAY</h5>
+          <p className="card-text">Best Popular cosmetics</p>
+        </div>
+        <div className="card-footer">
+          <button onClick={login} type="button" className="btn btn-secondary btn-lg btn-block">EYE PRIMER</button>
+          <img className="img-catalog" src={alis} alt="Card cap" />
+          <div className="card-body">
+          <h5 className="card-title">BENEFIT COSMETIC</h5>
+          <p className="card-text">Best Popular cosmetics</p>
+        </div>
+          <button onClick={login} type="button" className="btn btn-secondary btn-lg btn-block">ALIS</button>
+          </div>
+      </div>
+      <div className="card">
+        <img className="img-catalog" src={gambar2} alt="Card cap" />
+        <div className="card-body">
+          <h5 className="card-title">DIOR</h5>
+          <p className="card-text">Best Popular cosmetics</p>
+        </div>
+        <div className="card-footer">
+          <button onClick={login} type="button" className="btn btn-secondary btn-lg btn-block">LIPSTICK</button>
+          <img className="img-catalog" src={bedak} alt="Card cap" />
+          <div className="card-body">
+          <h5 className="card-title">DIOR</h5>
+          <p className="card-text">Best Popular cosmetics</p>
+        </div>
+          <button onClick={login} type="button" className="btn btn-secondary btn-lg btn-block">BEDAK</button>
+          
+        </div>
+      </div>
+      
+      <div className="card">
+        <img className="card-gambar3" src={gambar3} alt="Card cap" />
+        <div className="card-body">
+          <h5 className="card-title">DIOR FOUNDATION</h5>
+          <p className="card-text">A high perfection, long-wearing foundation formula that offers a 24-hour </p>
+        </div>
+        <div className="card-footer">
+          <button onClick={login} type="button" className="btn btn-secondary btn-lg btn-block">FOUNDATION</button>
+          <img className="img-catalog" src={perona} alt="Card cap" />
+          <div className="card-body">
+          <h5 className="card-title">BENEFIT COSMETICS</h5>
+          <p className="card-text">Best Popular cosmetics</p>
+        </div>
+          <button onClick={login} type="button" className="btn btn-secondary btn-lg btn-block">PERONA PIPI</button>
+        </div>
 
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <div className="nav">
+          <ul>
+            <li><Link to="/beranda">Beranda</Link>
+            </li>
+            <li>
+              <Link to="/produk">Product</Link>
+            </li>
+            <li>
+              <Link to="/profil">Profile</Link>
+            </li>
+            <AuthButton />
+          </ul>
+
+          <Switch>
+            <Route path="/beranda">
+              <BerandaPage />
+            </Route>
+            {/* <Route path="/login">
+              <LoginPage />
+            </Route> */}
+            <Route path="/skincare">
+              <skincarePage />
+            </Route>
+            <PrivateRoute path="/produk">
+              <ProtectedPage />
+            </PrivateRoute>
+            <PrivateRoute path="/private">
+              <ProtectedPage />
+            </PrivateRoute>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+function BerandaPage() {
   return (
     <div>
-      <p>You must log in to view the page at {from.pathname}</p>
-      <button onClick={login}>Log in</button>
+      <br />
+      <h1 align="center">SEPHORA</h1>
+      <p align="center">Ekspresikan Dirimu menggunakan Produk Kami</p>
     </div>
   );
 }
