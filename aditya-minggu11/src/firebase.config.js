f71ba80b4472609045db57587d0ea8c7878ac70d
AdityaPanca <aditya.annecdo@gmail.com>
2020-04-15 16:44:58 +0700

    Firebase minggu 11

---
+++ aditya-minggu11/src/firebase.config.js
@@ -0,0 +1,10 @@
+export default{
+    apiKey: "AIzaSyBbnFWapuaLOG03qxstOSG56WJOc831RXQ",
+    authDomain: "react-853ac.firebaseapp.com",
+    databaseURL: "https://react-853ac.firebaseio.com",
+    projectId: "react-853ac",
+    storageBucket: "react-853ac.appspot.com",
+    messagingSenderId: "254525469952",
+    appId: "1:254525469952:web:0d3e1a110f3d0ceec4afe5",
+    measurementId: "G-SR647HS3GP"
+};
